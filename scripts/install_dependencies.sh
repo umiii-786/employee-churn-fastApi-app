ec729cb139205a938cb4a628b3223d5137f96f58
Umair <umiiideveloper@gmail.com>
2026-03-16 20:21:19 +0500

    after setted the .yaml into yml v2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,21 +1,21 @@
 # Export Debian Frontend
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get update -y
-sudo apt-get install -y docker.io
+apt-get update -y
+apt-get install -y docker.io
 
 # start docker app
-sudo systemctl start docker
-sudo systemctl enable docker
+systemctl start docker
+systemctl enable docker
 
 
 # installing and downloading awscli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/home/ubuntu/awscliv2.zip" 
 unzip -o /home/ubuntu/awscliv2.zip -d /home/ubuntu/
-sudo /home/ubuntu/aws/install 
+ /home/ubuntu/aws/install 
 aws --version
 
-sudo usermod -aG docker ubuntu
+usermod -aG docker ubuntu
 
 # Login to ECR
 aws ecr get-login-password --region ap-south-1 \
