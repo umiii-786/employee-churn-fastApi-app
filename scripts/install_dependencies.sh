a01263f7c47564deeae8b0356d9b4caf12c0a708
Umair <umiiideveloper@gmail.com>
2026-03-16 15:05:17 +0500

    setted the code in .sh files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,43 +1,25 @@
-#!/bin/bash
+# Export Debian Frontend
+export DEBIAN_FRONTEND=noninteractive
 
-# Update packages
-sudo apt update -y
+sudo apt-get update -y
+sudo apt-get install -y docker.io
 
-# Install Docker
-sudo apt install docker.io -y
-
-cd /home/ubuntu
-
-# Start and enable Docker
+# start docker app
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# Add ubuntu user to docker group
+
+# installing and downloading awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/home/ubuntu/awscliv2.zip" 
+unzip -o /home/ubuntu/awscliv2.zip -d /home/ubuntu/
+sudo /home/ubuntu/aws/install 
+aws --version
+
 sudo usermod -aG docker ubuntu
 
-# Install dependencies
-sudo apt install unzip curl -y
+# Login to ECR
+aws ecr get-login-password --region ap-south-1 \
+| docker login --username AWS --password-stdin 528043283929.dkr.ecr.ap-south-1.amazonaws.com
 
-# Download AWS CLI v2
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-
-# Unzip installer
-unzip awscliv2.zip
-
-# Install AWS CLI
-sudo ./aws/install
-
-
-
-# install ruby
-sudo apt-get install ruby -y
-# downloading codedeploy agent
-wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
-sudo service codedeploy-agent start
-sudo systemctl enable codedeploy-agent
-
-
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 528043283929.dkr.ecr.ap-south-1.amazonaws.com
+# Pull docker image
 docker pull 528043283929.dkr.ecr.ap-south-1.amazonaws.com/churn-application-image:v1
